1b7de88ccde9f1157d021b1fbd13270a1c365517
Mattia Morea <mattia.morea@icloud.com>
2025-03-13 12:30:52 +0100

    feat: added tests for user and cart management

--- tests/tests_user.mjs
+++ tests/tests_user.mjs
@@ -1,5 +1,5 @@
 import {User, Session, SessionContainer} from "../classes/user.mjs"
-import { FoodItem, Bag, RegularBag } from "../classes/bag.mjs";
+import { FoodItem, Bag } from "../classes/bag.mjs";
 import dayjs from "dayjs";
 
 const user1 = new User("Bordello", 1234);
@@ -18,13 +18,13 @@ const fi3 = new FoodItem("Salmon Nigiri", 5);
 const fi4 = new FoodItem("Bao", 3);
 const fi5 = new FoodItem("Maki", 4);
 
-const rb1 = new Bag("Regular", [fi1, fi2, fi3], "medium", 12, "store", dayjs("2025-03-31"), dayjs("2025-04-01"));
-const rb2 = new Bag("Regular", [fi1, fi2, fi3, fi4, fi5], "large", 12, "store", dayjs("2025-03-31"), dayjs("2025-04-01"));
+const rb1 = new Bag("rb1", [fi1, fi2, fi3], "medium", 12, "store", dayjs("2025-03-31"), dayjs("2025-04-01"));
+const rb2 = new Bag("rb2", [fi1, fi2, fi3, fi4, fi5], "large", 12, "store", dayjs("2025-03-31"), dayjs("2025-04-01"));
 
 user1.addBag(rb2);
-user1.shopping_cart.list_bags.forEach(b => console.log(b.showItems()));
-user1.removeItem(fi1, rb2);
-user1.removeItem(fi2, rb2);
-console.log(rb2.removedItemsCounter);
-user1.shopping_cart.list_bags.forEach(b => console.log(b.showItems()));
+user1.shopping_cart.list_bags.forEach(b => console.log(b.food_items));
+user1.shopping_cart.removeItem(fi1, rb2);
+user1.shopping_cart.removeItem(fi2, rb2);
+console.log(`Removed ${rb2.removedItemsCounter} items from bag ${rb2.bag_type}.`);
+user1.shopping_cart.list_bags.forEach(b => console.log(b.food_items));
 user1.removeItem(fi3, rb2);
