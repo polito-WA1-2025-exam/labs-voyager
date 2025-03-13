706c9b90b2ce2e007cbf63934256586258eb444e
s298574@studenti.polito.it <s298574@studenti.polito.it>
2025-03-13 11:29:28 +0100

    improve code wrt common practices and standard

---
+++ tests/tests_bags.mjs
@@ -0,0 +1,7 @@
+import { SurpriseBag, RegularBag, FoodItem } from "../classes/bag.mjs";
+
+try{
+    const b1 = new SurpriseBag()
+} catch (error) {
+    console.log(error.message);
+}
--- tests/tests_user.mjs
+++ tests/tests_user.mjs
@@ -1,13 +1,13 @@
-import {User, Session, SessionContainer} from "../classes/user.mjs"
+import {User} from "../classes/user.mjs"
 
 const user1 = new User("Bordello", 1234);
-console.log(`Username: ${user1.username} Password: ${user1.password}`)
-let session1 = new Session(user1);
-console.log(session1)
-let sessions = new SessionContainer();
-sessions.add(session1)
-sessions.show_active().forEach(x => console.log(x))
-session1.terminate()
-sessions.show_active().forEach(x => console.log(x))
+
+console.log(user1.logIn().showStatus())
+
+try{
+    const uEmpty = new User();
+} catch (error) {
+    console.log(error.message);
+}
 
 
