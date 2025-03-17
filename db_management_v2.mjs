4b2e2b7a2e7cdcd4d36ff73f04c53b9305925a15
s298574@studenti.polito.it <s298574@studenti.polito.it>
2025-03-17 08:24:07 +0100

    add additional version based on promises, async and await to create and populate a sqlite db

---
+++ db_management_v2.mjs
@@ -0,0 +1,111 @@
+import sqlite from "sqlite3";
+
+const db_name = "./db2.sqlite";
+const db = new sqlite.Database(db_name, (err) => { if (err) throw err; });
+
+const runQuery = (sql, message) => {
+
+    return new Promise((resolve, reject) => {
+
+        db.run(sql, (err) => {
+            if (err) {
+                return reject(new Error(`SQL error: ${err.message}`));
+            }
+            resolve({success: true, message});
+        });
+    })
+}
+
+
+function businessTable() {
+
+    const sql = `CREATE TABLE IF NOT EXISTS business(
+        id INTEGER PRIMARY KEY,
+        name TEXT NOT NULL,
+        address TEXT NOT NULL,
+        phoneNumber INTEGER NOT NULL,
+        cuisineType TEXT,
+        foodCategory TEXT)`;
+
+    return runQuery(sql, "Business Table created.")
+};
+
+function bagTable() {
+
+    const sql = `CREATE TABLE IF NOT EXISTS bag(
+        id INTEGER PRIMARY KEY,
+        bagType TEXT NOT NULL,
+        size TEXT NOT NULL,
+        price REAL NOT NULL,
+        businessFrom INTEGER,
+        timestampStart TEXT NOT NULL,
+        timestampEnd TEXT NOT NULL,
+        removedItemsCounter INTEGER,
+        FOREIGN KEY(businessFrom) REFERENCES business(id))`;
+
+        return runQuery(sql, "Bag Table created.");
+}
+
+function foodTable() {
+
+    const sql = `CREATE TABLE IF NOT EXISTS fooditem(
+        id INTEGER PRIMARY KEY, 
+        name TEXT NOT NULL, 
+        quantity INTEGER NOT NULL, 
+        bagId INTEGER NOT NULL,
+        FOREIGN KEY(bagId) REFERENCES bag(id)
+    )`;
+
+    return runQuery(sql, "FoodItem Table created.");
+}
+
+function userTable(){
+
+    const sql = `CREATE TABLE IF NOT EXISTS user(
+        id INTEGER PRIMARY KEY, 
+        username TEXT NOT NULL, 
+        password TEXT NOT NULL, 
+        status INTEGER NOT NULL)`;
+
+    return runQuery(sql, "User Table created.");
+}
+
+async function main() {
+
+    try {
+
+        const stepCheck = (step) => {
+            if (!step.success){
+               throw new Error(step.message);
+            }
+            console.log(step.message);
+        }
+
+        const step1 = await userTable();
+        stepCheck(step1);
+        const step2 = await businessTable();
+        stepCheck(step2);
+        const step3 = await bagTable();
+        stepCheck(step3);
+        const step4 = await foodTable();
+        stepCheck(step4);
+
+    } catch (err) {
+        console.log(err.message);
+        
+    } finally {
+
+        if (db) {
+            db.close((err) => {
+                if (err) {
+                    console.error('Error closing the database connection:', err.message);
+                } else {
+                    console.log('Database connection closed.');
+                }
+            });
+        }
+    }
+}
+
+main();
+
---
+++ populate_db.mjs
@@ -0,0 +1,144 @@
+import sqlite from "sqlite3";
+import dayjs from "dayjs";
+
+import { Business } from "./classes/business.mjs";
+import { RegularBag, SurpriseBag, FoodItem } from "./classes/bag.mjs";
+import { User } from "./classes/user.mjs";
+
+const db_name = "./db2.sqlite";
+const db = new sqlite.Database(db_name, (err) => { if (err) throw err; });
+
+function addBusiness(name, address, phone_number, cuisine_type, food_category) {
+
+    const newBusiness = new Business(name, address, phone_number, cuisine_type, food_category);
+    const sql = 'INSERT INTO business(name, address, phoneNumber, cuisineType, foodCategory) VALUES (?, ?, ?, ?, ?)';
+
+    return new Promise((resolve, reject) => {
+
+        db.run(sql, [name, address, phone_number, cuisine_type, food_category], function(err){
+            if (err)
+                reject(err);
+            else {
+                newBusiness.id = this.lastID;
+                resolve(newBusiness);
+            }
+        });
+    });
+}
+
+function addUser(username, password){
+
+    const newUser = new User(username, password);
+    const sql = 'INSERT INTO user(username, password, status) VALUES (?, ?, ?)';
+
+    return new Promise((resolve, reject) => {
+
+        db.run(sql, [newUser.username, newUser.password, newUser.status], function(err){
+
+            if (err)
+                reject(err);
+            else{
+                newUser.id = this.lastID;
+                resolve(newUser)
+            }
+        });
+    });
+}
+
+const promiseBag = (b) => new Promise((resolve, reject) => {
+    const sqlBag = 'INSERT INTO bag(bagType, size, price, businessFrom, timestampStart, timestampEnd, removedItemsCounter) VALUES (?, ?, ?, ?, ?, ?, ?)';
+    // TODO: fix timestampStart, timestampEnd parameters to correctly insert the dates
+    
+    const counter = undefined || b.removedItemsCounter;
+    let inputs = [b.bag_type, b.size, b.price, b.business_from.id, b.timestamp_start, b.timestamp_end, counter];
+
+    db.run(sqlBag, inputs, function(err){
+
+        if (err)
+            reject(err);
+        else {
+            b.id = this.lastID;
+            resolve(b);
+        }
+    });
+})
+    
+
+const promiseFood = (f, b) => new Promise((resolve, reject) => {
+    const sqlFood = 'INSERT INTO fooditem(name, quantity, bagId) VALUES (?, ?, ?)';
+    db.run(sqlFood, [f.name, f.quantity, b.id], function(err){
+
+        if (err)
+            reject(err);
+        else {
+            f.id = this.lastID;
+            resolve(f);
+        }
+    })
+})
+    
+
+
+async function addBag(constructor, food_items, size, price, business_from, timestamp_start, timestamp_end) {
+
+    try {
+        let newBag = new constructor(
+            food_items, size, price, business_from, timestamp_start, timestamp_end
+        ) 
+
+        newBag = await promiseBag(newBag);
+        console.log("Add new bag -> id:"+newBag.id);
+
+        for (const item of food_items){
+
+            const addedFood = await promiseFood(item, newBag);
+            console.log("Add new food -> id:"+addedFood.id);
+        }
+
+        return newBag;
+    }
+    catch (err){
+
+        throw new Error("addBag error:"+EvalError.message);
+    }
+
+}
+
+
+async function main() {
+
+    try {
+
+        const bu1 = await addBusiness("Burger King", "Via X", "333", "fast food", "meat");
+        console.log("==> Add new business -> id:", bu1.id);
+
+        const u1 = await addUser("pippo", "012345");
+        console.log("==> Add new user -> id:"+u1.id);
+
+        const poke = new FoodItem("Poké", 1);
+        const salmon = new FoodItem("Salmon Nigiri", 5);
+        const bao = new FoodItem("Bao", 3);
+        const salmon2 = new FoodItem("Salmon Nigiri", 5);
+        const bao2 = new FoodItem("Bao", 3);
+
+        const bag1 = await addBag(
+            RegularBag, [poke, salmon, bao], "large", 15.90, bu1, "2025-03-31", "2025-04-01"
+        );
+        console.log("==> Add new bag -> id:"+bag1.id);
+        const bag2 = await addBag(
+            SurpriseBag, [salmon2, bao2], "large", 15.90, bu1, "2025-03-31", "2025-04-01"
+        );
+        console.log("==> Add new bag -> id:"+bag2.id);
+
+    } catch (err) {
+        console.log(err);
+    } finally {
+        db.close((err) => {
+
+            if (err) console.error(err.message);
+            console.log('Close the database connection.');
+        })
+    }
+}
+
+main();
