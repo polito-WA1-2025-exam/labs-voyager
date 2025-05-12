669865d6a5b3b22298cb35185e436f990bd30c88
Mattia Morea <mattia.morea@icloud.com>
2025-05-12 17:08:12 +0200

    add react-bootstrap dependencies [lab5]

--- surplus-food/src/App.jsx
+++ surplus-food/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './App.css';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
