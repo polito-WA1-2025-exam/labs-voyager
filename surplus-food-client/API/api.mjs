31f9520b4a285ad304c1088222c2b07f224b8eee
s346299 <s346299@studenti.polito.it>
2025-05-24 18:53:35 +0200

    feat: implemented error handling in client/api

--- surplus-food-client/API/api.mjs
+++ surplus-food-client/API/api.mjs
@@ -3,27 +3,59 @@ import { Business } from "../models/business.mjs";
 const URI = 'http://localhost:3000/api'
 
 async function loadEstablishments() {
-    const response = await fetch(URI + '/businesses');
-    const establishments = await response.json();
-    return establishments;
+    try {
+        const response = await fetch(URI + '/businesses');
+        if (response.ok) {
+            const establishments = await response.json();
+            return establishments;
+        } else {
+            throw new Error("Application error in loadEstablishments");
+        }
+    } catch (ex) {
+        throw new Error("Network error in loadEstablishments " + ex);
+    }
 }
 
 async function loadEstablishment(buId) {
-    const response = await fetch(URI + `/businesses/${buId}`);
-    const establishment = await response.json();
-    return establishment;
+    try {
+        const response = await fetch(URI + `/businesses/${buId}`);
+        if (response.ok) {
+            const establishment = await response.json();
+            return establishment;
+        } else {
+            throw new Error("Application error in loadEstablishment");
+        }
+    } catch (ex) {
+        throw new Error("Network error in loadEstablishment " + ex);
+    }
 }
 
 async function loadBagsFromEstablishment(buId) {
-    const response = await fetch(URI + `/businesses/${buId}/bags`);
-    const bags = await response.json();
-    return bags;
+    try {
+        const response = await fetch(URI + `/businesses/${buId}/bags`);
+        if (response.ok) {
+            const bags = await response.json();
+            return bags;
+        } else {
+            throw new Error("Application error in loadBagsFromEstablishment");
+        }
+    } catch (ex) {
+        throw new Error("Network error in loadBagsFromEstablishment " + ex);
+    }
 }
 
 async function loadFoodItemsOfBag(bagId) {
-    const response = await fetch(URI + `/bags/${bagId}/fooditems`);
-    const foodItems = await response.json();
-    return foodItems;
+    try {
+        const response = await fetch(URI + `/bags/${bagId}/fooditems`);
+        if (response.ok) {
+            const foodItems = await response.json();
+            return foodItems;
+        } else {
+            throw new Error("Application error in loadFoodItemsOfBag");
+        }
+    } catch (ex) {
+        throw new Error("Network error in loadFoodItemsOfBag " + ex);
+    }
 }
 
 export { loadEstablishments, loadEstablishment, loadBagsFromEstablishment, loadFoodItemsOfBag };
